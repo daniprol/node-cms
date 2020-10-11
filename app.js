31e8302954652a847f8ab8365fbc708d759e38af
Dani Prol <daniprol@hotmail.com>
2020-10-11 22:02:11 +0200

    Set handlebars as the view engine

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 const createError = require("http-errors");
 require("dotenv").config();
 
@@ -10,6 +11,10 @@ app.use(express.json()); // Configure the body parser with the express built-in 
 app.use(express.urlencoded({ extended: true })); // extended URLencoded middleware
 // express.static: This is a built-in middleware function in Express. It serves static files and is based on serve-static.
 app.use(express.static(path.join(__dirname, "public"))); // We need to serve the 'public' directory as static files
+
+/* Setup the view engine to use handlebars */
+app.engine("handlebars", hbs({ defaultLayout: "default" }));
+app.set("view engine", "handlebars");
 
 /* ROUTES */
 app.use("/", (req, res) => {

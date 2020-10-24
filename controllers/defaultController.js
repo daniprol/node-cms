ffd4898fb4554be75d98d36cba4c1469e184e58c
Dani Prol <daniprol@hotmail.com>
2020-10-24 20:42:27 +0200

    User registration

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,6 @@
 const layoutsController = require("./layoutsController");
 const Post = require("../models/PostModel");
+const User = require('../models/UserModel')
 const Category = require("../models/CategoryModel");
 
 module.exports = {
@@ -44,6 +45,55 @@ module.exports = {
     res.render("default/register", { layout: layoutsController.default });
   },
   registerPost: (req, res) => {
-    res.send("This is the POST register route");
+    let errors = []
+
+    if (!req.body.firstName) {
+      errors.push({ message: 'First name is mandatory'})
+    }
+    if (!req.body.lastName) {
+      errors.push({ message: 'Last name is mandatory'})
+    }
+    if (!req.body.email) {
+      errors.push({ message: 'Email is mandatory'})
+    }
+    if (req.body.password !== req.body.passwordConfirm) {
+      errors.push({ message: 'Passwords do not match each other'})
+    }
+
+    if (errors.length > 0) {
+
+      console.log('Registration form errors:', errors);
+      res.render('default/register', 
+      { layout: layoutsController.default, 
+        errors: errors, 
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName, 
+        email: req.body.email})
+    } else {
+      User.findOne({ email: req.body.email}).then(user => {
+        if (user) {
+          console.log(`Email ${req.body.email} is already in the db`);
+        req.flash('error-message', 'That email is already registered!')
+        res.redirect('/register')
+        } else {
+        //   const newUser = new User({
+        // firstName: req.body.firstName, 
+        // lastName: req.body.lastName, 
+        // email: req.body.email,
+        // password: req.body.password
+        //   })
+
+          // Can I use new User(req.body) if there's extra fields like req.body.passwordConfirm???
+          // Yes we can!
+          const newUser = new User(req.body)
+
+        newUser.save().then(user => {
+          console.log('New user registered', user);
+          req.flash('success-message', `${ user.email } account was created successfully. You can now log in`)
+          res.redirect('/login')
+        })
+        }
+      })
+    }
   },
 };
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema(
   {
@@ -24,6 +25,19 @@ const UserSchema = new Schema(
     timestamps: true,
   }
 );
+
+UserSchema.pre("save", async function (next) {
+  try {
+    console.log("Hashing the user password before saving to DB");
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 const UserModel = mongoose.model("user", UserSchema);
 
